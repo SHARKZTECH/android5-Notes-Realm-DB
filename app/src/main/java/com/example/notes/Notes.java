481ebe855a98bfc4982d60e15c25ae2f2649b4ad
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-09-04 23:17:34 +0300

    0

---
+++ app/src/main/java/com/example/notes/Notes.java
@@ -0,0 +1,31 @@
+package com.example.notes;
+
+public class Notes {
+    String title;
+    String description;
+    long createdTim;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCreatedTim() {
+        return createdTim;
+    }
+
+    public void setCreatedTim(long createdTim) {
+        this.createdTim = createdTim;
+    }
+}
