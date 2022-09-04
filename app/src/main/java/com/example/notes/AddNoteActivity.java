4d7f53cf66623d855004c5ca4b89d3778f086360
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-09-05 00:17:48 +0300

    0

--- app/src/main/java/com/example/notes/AddNoteActivity.java
+++ app/src/main/java/com/example/notes/AddNoteActivity.java
@@ -3,6 +3,10 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        EditText titileInp=findViewById(R.id.titleInp);
+        EditText descriptionInp=findViewById(R.id.DescriptionInp);
+        MaterialButton saveBtn=findViewById(R.id.saveBtn);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
